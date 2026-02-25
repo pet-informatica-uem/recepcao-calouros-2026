f6944d58d90bd3b3c0f9c63f724315010c82e86b
Guilherme <ra138642@uem.br>
2026-02-25 17:14:45 -0300

    arruma algumas informações das entidades, coloca o crt sobre as janelas e implementa foco das janelas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { HeroSection } from "../components/heroSection";
 import Header  from "../components/header";
-import { DADOSENTIDADES } from "./dadosJanelas";
 import AreaJanelas from "../components/areaJanelas";
 import { useState } from "react";
 import { entidadeKey } from "./dadosJanelas";
@@ -14,14 +13,19 @@ export default function Home() {
     if(!janelasAbertas.includes(id)){setJanelasAbertas([...janelasAbertas, id])}
   }
 
-    const fecharJanela = (id: entidadeKey) => {
-      setJanelasAbertas(janelasAbertas.filter(((i) => i != id)))
-    }
+  const fecharJanela = (id: entidadeKey) => {
+    setJanelasAbertas(janelasAbertas.filter(((i) => i != id)))
+  }
+
+  const focarJanela = (id: entidadeKey) => {
+    let aux = janelasAbertas.filter((item) => (item != id))
+    setJanelasAbertas([...aux, id])
+  }
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Header abrirJanela={abrirJanela}/>
-      <AreaJanelas entidades = {janelasAbertas} fecharJanela={fecharJanela}/>
+      <AreaJanelas entidades = {janelasAbertas} fecharJanela={fecharJanela} focarJanela={focarJanela}/>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <HeroSection/>
       </main>
