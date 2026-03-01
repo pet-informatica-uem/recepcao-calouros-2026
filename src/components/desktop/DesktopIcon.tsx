fad6c1089e2f94ad00c57a7f10bd2088c10f97c2
Victor Costa <vg47683@gmail.com>
2026-02-28 21:40:13 -0300

    responsividade do hero e do cronograma

--- src/components/desktop/DesktopIcon.tsx
+++ src/components/desktop/DesktopIcon.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 type DesktopIconProps = {
   label: string;
   onClick: () => void;
+  isActive?: boolean;
 };
 
-export default function DesktopIcon({ label, onClick }: DesktopIconProps) {
+export default function DesktopIcon({ label, onClick, isActive }: DesktopIconProps) {
   return (
     <button
       onClick={onClick}
@@ -13,7 +14,7 @@ export default function DesktopIcon({ label, onClick }: DesktopIconProps) {
     >
       <div className="w-16 h-16 flex items-center justify-center font-bold">
         <Image
-        src="/file-black.png"
+        src={isActive ? "/file-blue.png" : "/file-black.png"}
         alt="Ícone de arquivo de texto"
         width={460}
         height={570}
