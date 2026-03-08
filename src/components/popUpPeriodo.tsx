a61561fa4931c8c6fa4dbcb40f86b195dfd4053e
GabSeven <gabrodrigomes@gmail.com>
2026-03-08 17:41:33 -0300

    popUp pronto

--- src/components/popUpPeriodo.tsx
+++ src/components/popUpPeriodo.tsx
@@ -6,9 +6,12 @@ type popUpPeriodoProps = {
 
 export default function popUpPeriodo({ label }: popUpPeriodoProps) {
   return (
-    <div className="bg-white h-100 w-100 border-2  border-blue-600 text-blue-600 justify px-3 py-3">
-      <p className="text-left "> {label.name.toLowerCase()}.png </p>
-      <div className="bg-blue-600 h-full w-full text-white text-[80px]">
+    <div className="hidden md:flex bg-white h-80 w-80 flex-col border-2  border-blue-600 text-blue-600 px-3 py-3 absolute -left-75 translate-y-10">
+      <div className="flex flex-row justify-between">
+        <p className="text-left"> {label.name.toLowerCase()}.png </p>
+        <div className="text-3xl -translate-y-2">×</div>
+      </div>
+      <div className="bg-blue-600 flex-1 text-white text-[60px] flex flex-col justify-center">
         <p>{label.name.toUpperCase()} -&gt;</p>
       </div>
     </div>
